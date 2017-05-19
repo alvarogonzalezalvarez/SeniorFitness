ed0a473737d5330bb8f4b6d79d075e909873421f
Álvaro González Álvarez <alvaro.ga00@gmail.com>
2017-05-19 21:59:19 +0200

    F_Br Mejoras
    Mejoras flexiones de brazo.

--- app/src/main/java/com/alvaro/seniorfitness/activities/FuerzaBrazosActivity.java
+++ app/src/main/java/com/alvaro/seniorfitness/activities/FuerzaBrazosActivity.java
@@ -3,7 +3,10 @@ package com.alvaro.seniorfitness.activities;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alvaro.seniorfitness.R;
@@ -25,6 +30,8 @@ public class FuerzaBrazosActivity extends MainActivity {
     FuerzaBrazosListener listener;
     SensorManager sensorService;
     TextView repCount;
+    CountDownTimer countDown;
+    ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +47,10 @@ public class FuerzaBrazosActivity extends MainActivity {
         final Button startButton = (Button) findViewById(R.id.start);
         final Button stopButton = (Button) findViewById(R.id.stop);
         final Button resetButton = (Button) findViewById(R.id.reset);
+        TextView remainingTime = (TextView) findViewById(R.id.remainingTime);
         stopButton.setVisibility(View.GONE);
         resetButton.setVisibility(View.GONE);
+        remainingTime.setVisibility(View.GONE);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -56,7 +65,7 @@ public class FuerzaBrazosActivity extends MainActivity {
             public void onClick(View v) {
                 stop(v);
                 stopButton.setVisibility(View.GONE);
-                startButton.setVisibility(View.VISIBLE);
+                startButton.setVisibility(View.GONE);
             }
         });
 
@@ -70,18 +79,45 @@ public class FuerzaBrazosActivity extends MainActivity {
         });
     }
 
-    public void start(View view) {
+    public void start(final View view) {
         sensorService.registerListener(listener,
                 sensorService.getDefaultSensor(Sensor.TYPE_GRAVITY),
                 SensorManager.SENSOR_DELAY_FASTEST);
+
+        LinearLayout duracionText = (LinearLayout) findViewById(R.id.duracionText);
+        duracionText.setVisibility(View.GONE);
+        final TextView remainingTime = (TextView) findViewById(R.id.remainingTime);
+        remainingTime.setVisibility(View.VISIBLE);
+        EditText seconds = (EditText) findViewById(R.id.seconds);
+        countDown = new CountDownTimer(Integer.valueOf(seconds.getText().toString())*1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                remainingTime.setText(millisUntilFinished / 1000 + " segundos restantes");
+            }
+
+            public void onFinish() {
+                remainingTime.setText("TIEMPO!");
+                stop(view);
+                toneGen1.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD,400);
+                Button startButton = (Button) findViewById(R.id.start);
+                Button stopButton = (Button) findViewById(R.id.stop);
+                stopButton.setVisibility(View.GONE);
+                startButton.setVisibility(View.GONE);
+            }
+        }.start();
     }
 
     public void stop(View view) {
         sensorService.unregisterListener(listener);
+        countDown.cancel();
     }
 
     public void reset(View view) {
         stop(view);
+        countDown.cancel();
+        LinearLayout duracionText = (LinearLayout) findViewById(R.id.duracionText);
+        duracionText.setVisibility(View.VISIBLE);
+        TextView remainingTime = (TextView) findViewById(R.id.remainingTime);
+        remainingTime.setVisibility(View.GONE);
         repCount.setText("0");
         listener = new FuerzaBrazosListener(repCount, 8.0f, 0.0f);
     }
