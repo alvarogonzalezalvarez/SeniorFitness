b5dbeaf9a76aef67138fea2e6cbb6413bf78d0e2
Álvaro González Álvarez <alvaro.ga00@gmail.com>
2017-07-17 02:08:58 +0200

    Se listan las sesiones del usuario en la pantalla del perfil
    Se listan las sesiones del usuario en la pantalla del perfil

--- app/src/main/java/com/alvaro/seniorfitness/activities/UserDetailsActivity.java
+++ app/src/main/java/com/alvaro/seniorfitness/activities/UserDetailsActivity.java
@@ -1,15 +1,28 @@
 package com.alvaro.seniorfitness.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.alvaro.seniorfitness.R;
+import com.alvaro.seniorfitness.adapters.SessionsAdapter;
+import com.alvaro.seniorfitness.adapters.UsersAdapter;
+import com.alvaro.seniorfitness.database.SeniorFitnessContract;
 import com.alvaro.seniorfitness.database.SeniorFitnessDBHelper;
+import com.alvaro.seniorfitness.model.Session;
+import com.alvaro.seniorfitness.model.User;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,6 +41,7 @@ public class UserDetailsActivity extends AppCompatActivity {
     private String gender;
     private String birthdate;
     private String photo;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,7 @@ public class UserDetailsActivity extends AppCompatActivity {
         birthdate = getIntent().getStringExtra("birthdate");
         photo = getIntent().getStringExtra("photo");
         dbHelper = new SeniorFitnessDBHelper(this);
+        listView = (ListView) findViewById(R.id.listSessions);
         these = this;
 
         TextView nameView = (TextView) findViewById(R.id.user_profile_name);
@@ -81,6 +96,76 @@ public class UserDetailsActivity extends AppCompatActivity {
             }
         }
         photoView.setScaleType(ImageView.ScaleType.FIT_END);
+        new getSessions().execute();
+    }
+
+    private class getSessions extends AsyncTask<Void, Void, Session[]> {
+
+        @Override
+        protected Session[] doInBackground(Void... params) {
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+            // Definir la proyección de los datos que queremos, en este caso solo ID y primer nombre
+            String[] projection = {
+                    SeniorFitnessContract.Session.COLUMN_NAME_SESSIONID,
+                    SeniorFitnessContract.Session.COLUMN_NAME_USERID,
+                    SeniorFitnessContract.Session.COLUMN_NAME_ACTIVE,
+                    SeniorFitnessContract.Session.COLUMN_NAME_DATE
+            };
+
+            String where = SeniorFitnessContract.Session.COLUMN_NAME_USERID + " = ? AND ( " +
+                    SeniorFitnessContract.Session.COLUMN_NAME_ACTIVE + " = ? OR " +
+                    SeniorFitnessContract.Session.COLUMN_NAME_ACTIVE + " = ? " + ")";
+            String[] whereArgs = new String[] { userId, "TRUE", "COMPLETED" };
+
+            String sortOrder = SeniorFitnessContract.Session.COLUMN_NAME_SESSIONID + " DESC";
+
+            Cursor c = db.query(
+                    SeniorFitnessContract.Session.TABLE_NAME, // Nombre de la tabla
+                    projection,             // Columnas a devolver
+                    where,                  // Columnas para la cláusula WHERE
+                    whereArgs,              // Valores para la cláusula WHERE
+                    null,                   // GROUP BY
+                    null,                   // HAVING
+                    sortOrder);             // ORDER BY
+
+            // Recorrer la información devuelta
+            Session[] data = new Session[c.getCount()];
+            int i = 0;
+
+            while (c.moveToNext()) {
+                String sessionId = c.getString(c.getColumnIndex(SeniorFitnessContract.Session.COLUMN_NAME_SESSIONID));
+                String userId = c.getString(c.getColumnIndex(SeniorFitnessContract.Session.COLUMN_NAME_USERID));
+                String active = c.getString(c.getColumnIndex(SeniorFitnessContract.Session.COLUMN_NAME_ACTIVE));
+                String date = c.getString(c.getColumnIndex(SeniorFitnessContract.Session.COLUMN_NAME_DATE));
+                data[i++] = new Session(sessionId, userId, active, date);
+            }
+
+            c.close();
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(final Session[] sessions) {
+            if (sessions.length > 0) {
+                SessionsAdapter adapter = new SessionsAdapter(these, R.layout.session_list_element_even,
+                        sessions);
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Session theSession = sessions[position];
+                        if ("TRUE".equals(theSession.getActive())) {
+                            Intent intent = new Intent(these, SelectTestActivity.class);
+                            intent.putExtra("userId", theSession.getUserID());
+                            intent.putExtra("sessionId", theSession.getSessionID());
+                            startActivity(intent);
+                        }
+                    }
+                });
+            }
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/alvaro/seniorfitness/adapters/SessionsAdapter.java
@@ -0,0 +1,86 @@
+package com.alvaro.seniorfitness.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alvaro.seniorfitness.R;
+import com.alvaro.seniorfitness.model.Session;
+
+public class SessionsAdapter extends ArrayAdapter<Session> {
+
+    private Activity activity;
+
+    public SessionsAdapter(Activity activity, int resource, Session[] objects) {
+        super(activity, resource, objects);
+        this.activity = activity;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // return a value between 0 and (getViewTypeCount - 1)
+        return position % 2;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+
+        LayoutInflater inflater = (LayoutInflater) activity
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        int layoutResource = 0; // determined by view type
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case 0:
+                layoutResource = R.layout.session_list_element_even;
+                break;
+
+            case 1:
+                layoutResource = R.layout.session_list_element_odd;
+                break;
+        }
+
+        if (convertView == null) {
+            convertView = inflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        if ("TRUE".equals(getItem(position).getActive())) {
+            holder.sessiontext.setText(getItem(position).getDate() + ": Sesión en progreso");
+        } else {
+            holder.sessiontext.setText(getItem(position).getDate() + ": Sesión completada");
+            holder.continuesession.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        private TextView sessiontext;
+        private ImageView continuesession;
+
+        public ViewHolder(View v) {
+            sessiontext = (TextView) v.findViewById(R.id.sessiontext);
+            continuesession = (ImageView) v.findViewById(R.id.continuesession);
+        }
+    }
+}
